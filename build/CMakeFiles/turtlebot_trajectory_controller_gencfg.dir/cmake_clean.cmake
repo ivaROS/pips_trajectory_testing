f23f658af869e63fd39970ab714020c81805b49c
yipuzhao <yzhao347@gatech.edu>
2016-12-12 11:13:04 -0500

    remove build files

--- build/CMakeFiles/turtlebot_trajectory_controller_gencfg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/turtlebot_trajectory_controller_gencfg"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/turtlebot_trajectory_controller_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/yipuzhao/ros_workspace/package_dir/pips_trajectory_testing/build/devel/include;/home/yipuzhao/ros_workspace/package_dir/pips_trajectory_testing/include".split(';') if "/home/yipuzhao/ros_workspace/package_dir/pips_trajectory_testing/build/devel/include;/home/yipuzhao/ros_workspace/package_dir/pips_trajectory_testing/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "pips;trajectory_generator;turtlebot_trajectory_controller;dynamic_reconfigure".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "pips_trajectory_testing"
-PROJECT_SPACE_DIR = "/home/yipuzhao/ros_workspace/package_dir/pips_trajectory_testing/build/devel"
-PROJECT_VERSION = "0.0.0"
