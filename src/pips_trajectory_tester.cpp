e515f3d94c7a9f701cd800f7b041e9b94b8d5319
justin <jssmith@gatech.edu>
2017-09-27 19:23:18 -0400

    Changed some compile flags

--- src/pips_trajectory_tester.cpp
+++ src/pips_trajectory_tester.cpp
@@ -206,7 +206,7 @@ std::vector<ni_trajectory_ptr> GenAndTest::run(std::vector<traj_func_ptr>& traje
     auto t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
     
-    ROS_DEBUG_STREAM_NAMED(name_, "Generated " << num_paths << " trajectories in " << fp_ms.count() << " ms");
+    ROS_INFO_STREAM_NAMED(name_, "Generated " << num_paths << " trajectories in " << fp_ms.count() << " ms");
 
     return trajectories;
 }
